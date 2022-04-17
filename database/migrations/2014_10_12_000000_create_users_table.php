d2433e85589002296beb35790523b423a2d77c00
Sigma247 <khalildida93@gmail.com>
2022-04-17 01:18:15 +0000

    login + register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('prenom');
-            $table->string('telephone');
+            $table->string('name');
+            $table->string('prenom')->default('N/A');
+            $table->string('telephone')->default('N/A');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
